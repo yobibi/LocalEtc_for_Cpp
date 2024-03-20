30ac42092100fab9969aed64f3fa9a7fa0899b26
yobibi <mxesub2@gmail.com>
2024-03-20 10:11:11 +0900

    first commit

---
+++ ExampleLocalEtc/ExampleLocalEtc.cpp
@@ -0,0 +1,70 @@
+﻿// ExampleLocalEtc.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+// LocalEtcの使用例です。
+// cout/ifstream/ofstream を lcout/LocalIfs/LocalOfs に置き換えで文字コード処理を実現しています。
+//
+// このプログラムは文字コードを指定してファイルの読み書きを行う例です。引数はソース最後あたりを参照
+// コマンドライン動作で、Windows/Linux共通のソースです(C++17使用)
+//
+// Visual StudioはコンパイルのUTF-8設定(/utf-8)で文字定数の前につけるu8を省略
+// （u8を付けるとc++20では型指定も付けないとstringではエラーになるので積極的に省略）
+//
+
+#include "LocalEtc.hpp"
+using namespace LocalEtc;
+
+int main(int argc, char* argv[])
+{
+	// 標準出力／標準エラーの文字コードを変更する場合はコメントを外す
+	//std::string strSys = "UTF8";
+	//LSys.setSysUtfNum( LSys.getUtfNumFromStr(strSys) );
+
+	// 起動時引数取得
+    std::vector<std::string> listArg = LSys.getMainArg(argc, argv);
+
+	if ( listArg.size() == 4 ){		// ファイル書き込み処理
+		int numUtf = LSys.getUtfNumFromStr(listArg[2]);  // 文字列から文字コード番号取得
+		if ( numUtf < 0 ){		// 文字列異常
+			lcout << "文字コードが異常です : " << listArg[2] << std::endl;
+			return 1;
+		}
+		LocalOfs ofs;		// ofstreamをLocalOfsに置き換え
+		ofs.setUtfNum(numUtf);		// ファイルの文字コード設定（デフォルト設定で使う場合は不要）
+		ofs.open(listArg[1]);	// open
+//		ofs.open(listArg[1], std::ios::app);	// append
+		if ( !ofs ){
+			lcout << "書き込み失敗 : " << listArg[1] << std::endl;
+			return 1;
+		}
+		ofs << listArg[3] << std::endl;		// 書き込み
+		ofs.close();
+		// 名前空間は全部LocalEtcなので、LocalEtc::付加も可能
+		LocalEtc::lcout << "書き込み成功\n";
+	}
+	else if ( listArg.size() == 2 ){		// ファイル読み込み処理
+		LocalIfs ifs(listArg[1]);		// ifstreamをLocalIfsに置き換え
+		if ( !ifs ){  // 読み込み失敗
+			lcout << "ファイルがありません : " << listArg[1] << std::endl;
+			return 1;
+		}
+		std::string buf;
+		if ( !getline(ifs,buf) ){
+			lcout << "ファイル内にデータがありません\n";
+			 return 1;  // データなし
+		}
+		ifs.close();
+		int numUtf = ifs.getUtfNum();	// 読み込みファイルの文字コード取得（open以降で取得可能）
+		lcout << sformat("ファイルの文字コード選択は %s です\n", LSys.getUtfStrFromNum(numUtf).c_str());
+		lcout << "1行取得データ：" << buf << std::endl;
+		lcout << sformat("文字列長は %d です\n", LStr.getStrLen(buf));
+	}
+	else{
+		// UTF-8設定なければ定数文字列に u8 付加が必要
+		lcout << u8"ファイル名のみはread、出力文字列まで付加したらwrite\n";
+		lcout << "ExampleLocalEtc ファイル名 文字コード 出力文字列\n";
+		lcout << "ExampleLocalEtc ファイル名\n";
+		lcout << "  文字コード: STD UTF8 UTF16 SJIS UTF8N \n";
+		return 1;
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# LocalEtc_for_Cpp
-文字コード差異等を入出力で吸収するWindows/Linux共通C++支援プログラム
+﻿# LocalEtc for C++
+
+## 概要
+文字コード差異等を入出力で吸収する、Windows/Linux共通で使用できるC++支援プログラム
+
+実行動作例（ExampleLocalEtc）と一緒に格納しておく
+
+## 機能
+- **標準出力・標準エラー、ファイル読み書き** : 文字コードを指定したら後は関数が処理する
+- **OSアクセス（起動引数、ファイルCopy等、環境変数取得）** : Windows/Linux共通
+- **UTF-8文字列処理** : 内部はUTF-8のstringで動作するためUTF-8用の文字列関数追加
+- **一般関数** : 標準化された関数にないが広範囲で使いたい関数を追加
+- **以上をWindowsとLinuxで共通に使用できる**
+
+文字コードの対応については  
+- Shift-JIS / UTF-8（BOMあり・なし） / UTF-16（BOMあり）に対応
+- 文字コードの違いを吸収するため、内部はUTF-8のみで処理したらよい
+
+既存ソフト（join_logo_scp）Unicode対応のため作成したもので、
+既存ソースを最小限の変更で対応できるように考慮。
+
+## 使い方
+### ビルド方法
+1. C++17環境に`LocalEtc.cpp`, `LocalEtc.hpp`を追加する
+2. コンパイルでは`LocalEtc.cpp`も追加して、一緒にリンクする
+
+#### 実行動作例(ExampleLocalEtc)のビルドファイル
+- `ExampleLocalEtc.sln` (WindowsのVisual Studio 2022)
+- `makefile` （Linuxのmake）  
+
+### 記述
+1. `#include "LocalEtc.hpp"` を使用するソースに追加する
+2. 呼び出し先は全部LocalEtcの名前空間に存在するため、次のどちらかでアクセスする
+   - `using namespace LocalEtc;`を定義する
+   - 個別に`LocalEtc::`を付加して使用する
+3. コマンド詳細は`LocalEtc使用メモ.txt`を参照
+
+- 既存ソースに対応させる場合、cout/cerr/ifstream/ofstreamを使っていたら変更量が少なく対応しやすい
+- 文字列はstring変数で呼び出されること前提の作成（wstringではない。 << の使用が面倒）
+
+### コマンド記述例
+- 動作としては文字コードの違いを吸収で、内部動作はUTF-8に統一している
+- 定数文字列はUTF-8設定してu8は省略。省略せずu8を付けるとc++20では型指定も付けないとstringはエラーになる
+
+標準出力に事前設定された文字コードで出力（coutをlcoutに変更で対応）  
+```c++
+lcout << "文字列出力" << std::endl
+```
+
+指定文字コードでファイル出力（ofstreamをLocalOfsに変更で対応。文字コード設定部分のみ追加）  
+```c++
+string fname = "ファイル名.txt";
+LocalOfs ofs;
+int num = LSys.getUtfNumFromStr("UTF8"); // デフォルト文字コードから変更する場合の追加行
+ofs.setUtfNum(num);      // デフォルト文字コードから変更する場合の追加行
+ofs.open(fname);
+if ( !ofs ) return;
+string buf = "書き込みデータ\n";
+ofs << buf;
+ofs.close();
+```
+
+文字コードを自動認識してファイル読み込み（ifstreamをLocalIfsに変更で対応）
+```c++
+string fname = "ファイル名.txt";
+LocalIfs ifs(fname);
+if ( !ifs ) return;
+string buf;
+if ( !getline(ifs,buf) ) return;  // データなしはreturn
+ifs.close();
+```
+
+環境変数取得（LSys.（名前空間はLocalEtc）はOS・文字コード違いを吸収する処理）
+```c++
+string strVal;
+bool success = LSys.getEnvString(strVal, "CLI_IN_PATH");
+```
+## ライセンス
+自由に役立てたらと思いますが、明確化して MITライセンス で提供します。
